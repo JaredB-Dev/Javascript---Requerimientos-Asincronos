50952308f56f9f79322bfd63703d59bf0eee6274
Jared Selim Beltran Martinez <jarbeltr@espol.edu.ec>
2026-06-17 19:01:36 -0500

    solo realice un requerimiento asíncrono

---
+++ js/requirements.js
@@ -0,0 +1,5 @@
+
+let getSalesCoffee = async () => {
+    const response = await fetch("https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Coffee/Coffe_sales.xml");
+    return response
+}
